4648c35782f3d1fab5fc3d01fe28d2f694e31c5d
daniel-manrique <danielmanriquecastano@gmail.com>
2024-09-05 11:44:32 -0400

    Upload files

---
+++ src/disciplines/Astronomy.js
@@ -0,0 +1,21 @@
+const Astronomy = {
+    'General Information': [
+        { name: 'Title of the dataset', type: 'small' },
+        { name: 'Description', type: 'large' },
+        { name: 'Date of data collection', type: 'small' },
+    ],
+    'Author Information': [
+        { name: 'Name (Last name, name)', type: 'small' },
+        { name: 'Institution', type: 'small' },
+        { name: 'Email', type: 'small' },
+        { name: 'ORCID ID', type: 'small' },
+    ],
+    'Methodological Details': [
+        { name: 'Telescope used', type: 'small' },
+        { name: 'Equipment Used', type: 'small' },
+        { name: 'Procedure', type: 'large' },
+        { name: 'Analysis', type: 'large' }
+    ]
+};
+
+export default Astronomy;
